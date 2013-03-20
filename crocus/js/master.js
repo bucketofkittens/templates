93bc7fbe00963f36bf3b184360fa5351df463cfc
kittey <eir-keth@mail.ru>
2013-03-20 11:11:31 +0400

    то ие

--- crocus/js/master.js
+++ crocus/js/master.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
 	$('.scroll-pane').jScrollPane({
 		horizontalDragMaxWidth: 89
 	});
+
+	console.log(window.PIE);
+	if (window.PIE) {
+        $('#page #content .banner p, #page #content .banner ul li, #top-header-bg, #search').each(function() {
+            PIE.attach(this);
+        });
+    }
 });
