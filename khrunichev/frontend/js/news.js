a431cbb8e03d3911d5b7a26891a088a1b5701c3b
eir <eir-keth@mail.ru>
2013-06-24 17:02:18 +0400

    pgn news

--- khrunichev/frontend/js/news.js
+++ khrunichev/frontend/js/news.js
@@ -1,12 +1,12 @@
 var NewsView = Backbone.View.extend({
   className: 'news',
   newsnav: null,
-  newsbox: null,
+  newsblue: null,
 
   initialize: function () {
     this.template = $('#news-template').html();
     this.newsnav = new NewsnavView();
-    this.newsbox = new NewsboxView();
+    this.newsblue = new NewsblueView();
   },
 
   render: function () {
@@ -15,8 +15,8 @@ var NewsView = Backbone.View.extend({
   	$(this.el).append(this.newsnav.el);
   	this.newsnav.setElement(this.$(".newsnav-widget")).render();
 
-  	$(this.el).append(this.newsbox.el);
-  	this.newsbox.setElement(this.$(".newsbox-widget")).render();
+  	$(this.el).append(this.newsblue.el);
+  	this.newsblue.setElement(this.$(".newsblue-widget")).render();
    
     return this;
   },
@@ -36,10 +36,36 @@ var NewsnavView = Backbone.View.extend({
   }
 });
 
+var NewsblueView = Backbone.View.extend({
+  className: 'newsblue',
+  newsbox: null,
+  pgn: null,
+
+  initialize: function () {
+    this.template = $('#newsblue-template').html();
+    this.newsbox = new NewsboxView();
+    this.pgn = new PgnView();
+  },
+
+  render: function () {
+    $(this.el).html(_.template(this.template, this.context));
+
+    $(this.el).append(this.newsbox.el);
+    this.newsbox.setElement(this.$(".newsbox-widget")).render();
+
+    $(this.el).append(this.pgn.el);
+    this.pgn.setElement(this.$(".pgn-widget")).render();
+    
+    return this;
+  }
+});
+
 var NewsboxView = Backbone.View.extend({
   className: 'newsbox',
 
   initialize: function () {
+    this.newsList = new NewsList();
+    this.newsList.fetch();
     this.template = $('#newsbox-template').html();
   },
 
@@ -48,4 +74,25 @@ var NewsboxView = Backbone.View.extend({
     
     return this;
   }
+});
+
+var PgnView = Backbone.View.extend({
+  className: 'pgn',
+
+  initialize: function () {
+    this.template = $('#pgn-template').html();
+  },
+
+  render: function () {
+    $(this.el).html(_.template(this.template, this.context));
+    
+    return this;
+  }
+});
+
+var News = Backbone.Model.extend();
+
+var NewsList = Backbone.Collection.extend({
+   model: News,
+   url: 'http://localhost:4000/api/news'
 });
