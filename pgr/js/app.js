442114ac4c691702681410622f5c1c1f6d906964
eir <eir-keth@mail.ru>
2013-08-05 18:48:32 +0400

    redo

--- pgr/js/app.js
+++ pgr/js/app.js
@@ -3,5 +3,6 @@
 var pgrModule = angular.module('pgrModule', []);
 pgrModule.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/profile', {templateUrl: 'views/profile.html', controller: ProfileController});
+	$routeProvider.when('/', {templateUrl: 'views/main.html', controller: ProfileController});
 	$routeProvider.otherwise({redirectTo: '/'});
 }])
