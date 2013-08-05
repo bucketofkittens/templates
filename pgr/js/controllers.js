bfca2d1b6ab9229323c656f7f23b7492ccf95bf9
eir <eir-keth@mail.ru>
2013-08-05 20:49:35 +0400

    fjgndxlf

--- pgr/js/controllers.js
+++ pgr/js/controllers.js
@@ -1,4 +1,16 @@
 'use strict';
 
+function navCtrl($scope) {
+	$scope.items = {
+		{name: 'ABOUT', link: '#', active: false},
+		{name: 'LEAGUES', link: '#', active: false},
+		{name: 'PROFILE', link: '#/profile', active: false}
+	};
+	$scope.$on("$routeChangeStart", function () {
+		console.log("leaving DemoCtrl");
+	});
+}
+
 function ProfileController($scope) {
+
 }
