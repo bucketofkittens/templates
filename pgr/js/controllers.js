92b88228c1468ed90e5181347fbb32e73a637022
Andrey Makar <lyykfi@gmail.com>
2013-08-06 11:53:37 +0400

    комментарии

--- pgr/js/controllers.js
+++ pgr/js/controllers.js
@@ -1,11 +1,18 @@
 'use strict';
 
 function navCtrl($scope) {
+	/**
+	 * @TODO Потом нужно будет переменовать activate в className например.
+	 */
 	$scope.navs = [
 		{name: 'ABOUT', link: '#', active: ''},
 		{name: 'LEAGUES', link: '#', active: ''},
 		{name: 'PROFILE', link: '#/profile', active: ''}
 	];
+
+	/**
+	 * @TODO Нужно будет потом оптимизировать
+	 */
 	$scope.onNavClick = function($event, name) {
 		angular.forEach($scope.navs, function(value, key) {
 			if($scope.navs[key].name == name) {
