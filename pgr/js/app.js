ad9d9803cfb7bc7e02f553a215a0feacd7086a8d
Andrey <lyykfi@gmail.com>
2013-08-07 17:15:00 +0400

    куча правок по странице профиля

--- pgr/js/app.js
+++ pgr/js/app.js
@@ -4,11 +4,7 @@
  * Основной модуль приложения
  * @type {[type]}
  */
-<<<<<<< HEAD
-var pgrModule = angular.module('pgrModule', [ "ngResource", "localization" ]);
-=======
-var pgrModule = angular.module('pgrModule', [ "ngResource", "ui", "ui.keypress" ]);
->>>>>>> куча правок по странице профиля
+var pgrModule = angular.module('pgrModule', [ "ngResource", "localization", "ui", "ui.keypress" ]);
 
 /**
  * Роутинг приложения
@@ -20,3 +16,4 @@ pgrModule.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/profile/:userId', {templateUrl: 'views/profile.html', controller: ProfileController});
 	$routeProvider.when('/', {templateUrl: 'views/main.html', controller: ProfileController});
 	$routeProvider.otherwise({redirectTo: '/'});
+}]);
