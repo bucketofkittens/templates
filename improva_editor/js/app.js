61fa10009457fb4ec3b10a3a47d4fa4a3ad971d0
eir <eir-keth@mail.ru>
2013-09-30 19:09:24 +0400

    improva skelet

--- improva_editor/js/app.js
+++ improva_editor/js/app.js
@@ -1,36 +1,49 @@
 'use strict';
 
-/**
- * Основной модуль приложения
- * @type {[type]}
- */
+/** Основной модуль приложения */
 var editorModule = angular.module('editorModule', [ "ngRoute", "ngCookies", "ngResource", "ngAnimate", 'route-segment', 'view-segment']);
 
-/**
- * Роутинг приложения
- * @param  {[type]} $routeProvider [description]
- * @return {[type]}                [description]
- */
+/** Роутинг приложения */
 editorModule.config(function($routeSegmentProvider, $routeProvider) {
 	$routeSegmentProvider.options.autoLoadTemplates = true;
 
 	$routeSegmentProvider
     
         .when('/users', 'users')
-        .when('/users/:id',          'userInfo')
-        .when('/',      'main')
+        .when('/users/:id', 'userInfo')
+        .when('/', 'main')
         
         .segment('main', {
-            templateUrl: 'views/main.html',
+            templateUrl: 'views/home.html',
             controller: MainController})
 
         .segment('users', {
             templateUrl: 'views/users.html',
             controller: UsersController})
 
-        .segment('userInfo', {
-            templateUrl: 'views/userInfo.html',
-            controller: UserInfoController});
+        .segment('providers', {
+            templateUrl: 'views/providers.html',
+            controller: ProvidersController})
+
+        .segment('goalslib', {
+            templateUrl: 'views/goalslib.html',
+            controller: GoalslibController})
+
+        .segment('nsiedit', {
+            templateUrl: 'views/nsiedit.html',
+            controller: NsieditController})
+
+        .segment('mindmaps', {
+            templateUrl: 'views/mindmaps.html',
+            controller: MindmapsController})
+
+        .segment('charts', {
+            templateUrl: 'views/charts.html',
+            controller: ChartsController})
+
+        .segment('tutors', {
+            templateUrl: 'views/tutors.html',
+            controller: TutorsController});
 
 	$routeProvider.otherwise({ redirectTo: '/' });
 });
--- improva_editor/js/controllers.js
+++ improva_editor/js/controllers.js
@@ -1,3 +1,45 @@
+function navCtrl($scope, $location, $rootScope, $route) {
+	
+	$scope.hidden = false;
+
+
+	/** роутинг */
+	$scope.onNavClick = function($event, nav) {
+		if(nav.link) {
+		   $location.path(nav.link.replace('#', ''));
+		}
+		else {
+		   nav.onClick();
+		}
+	};
+
+
+	/** меню навигации */
+	$scope.generateNav = function() {
+		$scope.navs = [
+		   {name: "HOME", link: '#/home', activeClass: ''},
+		   {name: "USERS", link: '#/users', activeClass: ''},
+		   {name: "PROVIDERS", link: '#/providers', activeClass: ''},
+		   {name: "GOALS LIBRARY", link: '#/goalslib', activeClass: ''},
+		   {name: "NSI EDITOR", link: '#/nsiedit', activeClass: ''},
+		   {name: "MINDMAPS", link: '#/mindmaps', activeClass: ''},
+		   {name: "CHARTS", link: '#/charts', activeClass: ''},
+		   {name: "TUTORS", link: '#/tutors', activeClass: ''}
+		];
+	};
+	$scope.generateNav();
+
+
+	/** Событие при переходе по роутингу */
+	$scope.$on('$routeChangeSuccess', function(event, next, current) {
+		angular.forEach($scope.navs, function(value, key) {
+		   if($scope.navs[key].link) {
+		      $scope.navs[key].activeClass = $scope.navs[key].link.split("/")[1] === $location.path().split("/")[1] ? 'current' : '';
+		   }
+		});
+	});
+}
+
 function MainController($scope) {
 
 }
@@ -6,10 +48,30 @@ function UsersController($scope) {
 
 }
 
-function UserInfoController($scope) {
+function ProvidersController($scope) {
+    
+}
+
+function GoalslibController($scope) {
+
+}
+
+function NsieditController($scope) {
+    
+}
+
+function MindmapsController($scope) {
+
+}
+
+function ChartsController($scope) {
+    
+}
+
+function TutorsController($scope) {
 
 }
 
 function RootController($scope) {
-    
+
 }
