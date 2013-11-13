ba39c39d4910794224ad852222c6b14d37d7d545
Andrey Makar <lyykfi@gmail.com>
2013-11-14 01:12:04 +0400

    правки по верстке

--- regioncom/scripts/main.js
+++ regioncom/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	function setSizeMainImage() {
+		var imageWidth = $("#images_list").width()/3-10;
+		$.each($("#images_list img"), function(key, value) {
+			$(value).width(imageWidth);
+			$(value).next().width(imageWidth);
+		});	
+	}
+
+	setSizeMainImage();
+
+	$(window).resize(function() {
+		setSizeMainImage();
+	});
+	
+});
