93f571d44bce47ef7c7735000d31f6c9c076e781
lyykfi <lyykfi@gmail.com>
2013-12-20 17:25:26 +0400

    уффф

--- pgr/js/libs/directives.js
+++ pgr/js/libs/directives.js
@@ -54,6 +54,20 @@ pgrModule.directive('backImg', function() {
   }
 })
 
+
+
+pgrModule.directive('scroller', function($window) {
+  return {
+    link: function(scope, element, attrs) {
+      var footerSize = 70;
+      $(element).height($(window).height()-$(element).offset().top-footerSize);
+      $(window).resize(function() {
+        $(element).height($(window).height()-$(element).offset().top-footerSize);
+      });
+    }
+  }
+})
+
 pgrModule.directive('bridge', function($window) {
   return {
     link: function(scope, element, attrs) {
