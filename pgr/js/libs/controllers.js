0c08f3fda1fcf56be0baeb96ff6daa2de9b0af95
lyykfi <lyykfi@gmail.com>
2013-12-27 14:41:55 +0400

    восстановил работоспособность лиг

--- pgr/js/libs/controllers.js
+++ pgr/js/libs/controllers.js
@@ -1798,13 +1798,18 @@ function MainController($scope, Leagues, User, $rootScope, $location, $timeout, 
 
 /** Контроллер графика */
 function GraphsController($scope, $rootScope, $route, $location, Leagues, User) {
+    /**
+     * Сурово и беспощадно.
+     * Надо будет переписать
+     * @return {[type]} [description]
+     */
     setInterval(function() {
         var key = 0;
         $.each($("#graphs tr"), function(key, value){ 
             key += 1;
             $.each($(value).find("td"), function(keyd, valued){
                 var a = 10;
-                var points = $(valued).attr("data-points");
+                var points = parseInt($(valued).attr("data-points"));
                 var step = ($(valued).attr("data-step"))*10000;
                 if(points) {
                     if(points != 0) {
@@ -1821,18 +1826,23 @@ function GraphsController($scope, $rootScope, $route, $location, Leagues, User) 
         })
     }, 100);
 
+    /**
+     * Забираем список всех лиг
+     * @param  {[type]} data [description]
+     * @return {[type]}      [description]
+     */
     Leagues.query({}, {}, function(data){
         $scope.leagues = data;
 
+        /**
+         * Забираем пользователей для каждой лиги
+         * @param  {[type]} value [description]
+         * @param  {[type]} key   [description]
+         * @return {[type]}       [description]
+         */
         angular.forEach($scope.leagues, function(value, key){
             User.by_league({league_guid:value.sguid}, {}, function(v2, k2){
-                var user2 = [];
-                angular.forEach(v2, function(value, key){
-                    if(value.published == 1) {
-                        user2.push(value);
-                    }
-                });
-                var users = user2.splice(0,10);
+                var users = v2.splice(0,10);
 
                 if(users.length < 10) {
                     var i = 0;
@@ -1840,7 +1850,6 @@ function GraphsController($scope, $rootScope, $route, $location, Leagues, User) 
                         users.push({});
                     }
                 }
-                
                 value.users = users;
             })
         });
