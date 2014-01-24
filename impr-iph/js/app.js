9e7d42efd958ec2346dd3fb9d39b5e698357aa2a
lyykfi <lyykfi@gmail.com>
2014-01-24 14:15:44 +0400

    правки

--- impr-iph/js/app.js
+++ impr-iph/js/app.js
@@ -1,7 +1,15 @@
+var scrollStep = 0;
+
 $(document).ready(function() {
 	$(".page").css("min-height", $(window).height());
 
-	$("#iphone_page").scroll(function() {
-		alert("scroll");
-	})
+	document.addEventListener('touchmove', function(e) {
+		e.preventDefault();
+		alert(e);
+	}, true);
+	/*
+	$(document).scroll(function() {
+		scrollStep += 1;
+		$("html body").animate({ scrollTop: $(window).height()*scrollStep }, 600);
+	})*/
 })
