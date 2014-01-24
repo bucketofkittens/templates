7197342cca6f8a787c90c9ffb09f7a4cec085fa5
lyykfi <lyykfi@gmail.com>
2014-01-24 15:48:58 +0400

    правка

--- impr-iph/js/app.js
+++ impr-iph/js/app.js
@@ -1,14 +1,40 @@
 var scrollStep = 0;
 
-$(document).ready(function() {
-	$(".page").css("min-height", $(window).height());
+function slide() {
+	if(scrollStep < 0) {
+		scrollStep = 0;
+	}
+	$("#content").animate({
+		top: "-"+(scrollStep*$(window).height())+"px"
+	}, 600);
+}
 
-	$(document).bind('swipemove',function() {
-		console.log("swipe");
-	});
-	/*
-	$(document).scroll(function() {
-		scrollStep += 1;
-		$("html body").animate({ scrollTop: $(window).height()*scrollStep }, 600);
-	})*/
-})
+function slideIphone() {
+	if(scrollStep < 0) {
+		scrollStep = 0;
+	}
+	$("#content").animate({
+		top: "-"+(scrollStep*$(window).height())+"px"
+	}, 600);
+}
+
+$(document).ready(function() {
+	$(".page").css("height", $(window).height());
+
+
+	var element = document.getElementById('content');
+	Hammer(element).on("dragend", function(event) {
+		if(scrollStep == 2) {
+
+		} else {
+			if(event.gesture.direction == "up") {
+				scrollStep += 1;
+			} else {
+				scrollStep -= 1;
+			}
+	    	//scrollStep += 1;
+	        slide();	
+		}
+		
+    });
+});
