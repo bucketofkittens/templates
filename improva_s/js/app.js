a3c92d8ecb0b17367cfb1954a1368002d3f78d2f
lyykfi <lyykfi@gmail.com>
2014-01-24 16:19:02 +0400

    правки

--- improva_s/js/app.js
+++ improva_s/js/app.js
@@ -1,7 +1,8 @@
 if(navigator.userAgent.match(/iPhone/i)) {
-	alert(window.location);
 	window.location = window.location+"iphone/";
 }
+
+var scrollStep = 0;
 
 function scrollFix_() {
 	if($("body").scrollTop() > $("#main").height()) {
@@ -11,42 +12,84 @@ function scrollFix_() {
 	}
 }
 
+
+function scr() {
+	scrollStep = 0;
+	scroll();
+}
+
+function scroll() {
+	if(scrollStep < 0) {
+		scrollStep = 0;
+	}
+	if(scrollStep > 2) {
+		scrollStep = 2;
+	}
+	if(scrollStep > 1) {
+		$("header.while").addClass("show");
+	} else {
+		$("header.while").removeClass("show");
+	}
+	$("#content").animate({
+		top: "-"+(scrollStep*$(window).height())+"px"
+	}, 600);
+}
+
 $(document).ready(function() {
-
-
 	$(".page").css("min-height", $(window).height()-100);
 
 	$(window).resize(function() {
 		$(".page").css("min-height", $(window).height()-100);
 	});
 
+	$(document).bind('mousewheel', function(e){
+	    if(e.deltaY < 0) {
+	    	scrollStep += 1;
+	    } else {
+	    	scrollStep -= 1;
+	    }
+	    scroll();
+	 });
+
 	$('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top
-	        }, 1000);
-	        return false;
+	      var name = this.hash.slice(1);
+	      if(name == "about") {
+	      	scrollStep = 1;
 	      }
+	      if(name == "contact") {
+	      	scrollStep = 2;
+	      }
+	      scroll();
+	      return false;
 	    }
 	  });
+
+	var element = document.getElementById('content');
+	Hammer(element).on("dragend", function(event) {
+		if(event.gesture.direction == "up") {
+			scrollStep += 1;
+		} else {
+			scrollStep -= 1;
+		}
+    	//scrollStep += 1;
+    	setTimeout(function() {
+    		scroll();	
+    	}, 0)
+        	
+		
+    });
+
+
 
     var isiPad = navigator.userAgent.match(/iPad/i) != null;
     if(isiPad) {
     	$(".iphone_app").hide();
     }
 
-    scrollFix_();
 
-    $(document).scroll(function() {
-    	scrollFix_();
-	});
-
-	$("#content").on("touchmove", function(e) {
-		scrollFix_();
-	});
 
 	$('#socials').share({
         networks: ['facebook','googleplus','twitter']
