e4ded82721d32e30bde096f57ce3df206274bf0b
lyykfi <lyykfi@gmail.com>
2014-03-06 22:22:46 +0400

    sdf

--- intec/js/main.js
+++ intec/js/main.js
@@ -11,12 +11,20 @@ $(document).ready(function() {
 
 	proportion();
 
+	var texts = {
+		"project": "Холдинг объединяет <br />высокотехнологичные компании, <br />успешно работающие на IT-рынке <br />Российской Федерации",
+		"client": "Компании Холдинга заслужили высокую репутацию как надежные поставщики, обладающие глубокой профессиональной экспертизойв сфере системной интеграции, разработки программного обеспеченияна заказ и информатизации целого ряда отраслей",
+		"contact": "Сегодня компании Холдинга представлены в трех крупнейших субъектахРоссийской Федерации: в Москве, Санкт-Петербурге и Новосибирской области. Представительства компаний группы, партнеры и агенты работают на всей территории Российской Федерации."
+	};
+
 	$("body #content ul li").on("click", function() {
 		$("body #content ul li").removeClass("current");
 		$(this).addClass("current");
 
 		$("header nav a").removeClass("current");
 		$("header nav a[target='"+$(this).attr("id")+"']").addClass("current");
+
+		$("header h1").html(texts[$(this).attr("id")]);
 	});
 
 	$("header nav a").on("click", function() {
@@ -25,5 +33,7 @@ $(document).ready(function() {
 
 		$("header nav a").removeClass("current");
 		$(this).addClass("current");
+
+		$("header h1").html(texts[$(this).attr("target")]);
 	});
 });
