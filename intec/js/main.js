0bc03b14c2529f9d9a8ba6d6855d8971ae24d458
lyykfi <lyykfi@gmail.com>
2014-03-06 20:53:38 +0400

    sdf

--- intec/js/main.js
+++ intec/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	function proportion() {
 		$("body #content > ul > li, body #content > ul").css("height", $(window).height()-250);
-		$("body #content > ul").css("width", ($(window).height()-250)*2.5);
+		$("body #content > ul").css("width", ($(window).height()-250)*2.7);
 	}
 
 	$(window).resize(function() {
