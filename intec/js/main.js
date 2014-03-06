479a834cd3e21966d2e45c82725a48f4fe48b9d3
eir <eir-keth@mail.ru>
2014-03-06 20:48:21 +0400

    whites and discs

--- intec/js/main.js
+++ intec/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
 	function proportion() {
-		$("body #content ul li, body #content ul").css("height", $(window).height()-250);
-		$("body #content ul").css("width", ($(window).height()-250)*2.5);
+		$("body #content > ul > li, body #content > ul").css("height", $(window).height()-250);
+		$("body #content > ul").css("width", ($(window).height()-250)*2.5);
 	}
 
 	$(window).resize(function() {
